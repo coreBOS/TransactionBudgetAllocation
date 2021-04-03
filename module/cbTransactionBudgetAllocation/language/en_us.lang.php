a6b25670c6c5e839c5771a56bf997ccaa8852955
glorymoshi <glorysimon95@gmail.com>
2021-04-03 07:57:07 +0300

    feat(module)create module TransactionBudgetAllocation

---
+++ module/cbTransactionBudgetAllocation/language/en_us.lang.php
@@ -0,0 +1,33 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'cbTransactionBudgetAllocation' => 'TransactionBudgetAllocations',
+	'SINGLE_cbTransactionBudgetAllocation' => 'TransactionBudgetAllocation',
+	'TransactionBudgetAllocation ID' => 'TransactionBudgetAllocation ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_cbTransactionBudgetAllocation_INFORMATION' => 'TransactionBudgetAllocation Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'TransactionBudgetAllocation' => 'TransactionBudgetAllocation',
+	'Transaction Budget Allocation No' => 'Transaction Budget Allocation No',
+	'Related To' => 'Related To',
+	'Budget Item' => 'Budget Item',
+	'Percentage' => 'Percentage',
+	'Allocated Amount' => 'Allocated Amount',
+	'Assigned To' => 'Assigned To',
+	'Created By' => 'Created By',
+	'Created Time' => 'Created Time',
+	'Modified Time' => 'Modified Time',
+	'Description' => 'Description',
+
+);
+?>
